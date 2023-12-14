7b6288c91423a7591a9608a27d894dfc2fa45dd8
pooran-meena <153830513+pooran-meena@users.noreply.github.com>
2023-12-14 19:33:57 +0530

    Delete get_cli_arc/get_arc directory

--- get_cli_arc/get_arc/__brick__/{{name.snakeCase()}}/view/widgets/{{name.snakeCase()}}_widgets.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class {{name.pascalCase()}}Widget{
-  static Widget body({required String title}){
-    return Center(
-      child: Text('$title Page'),
-    );
-  }
-}
