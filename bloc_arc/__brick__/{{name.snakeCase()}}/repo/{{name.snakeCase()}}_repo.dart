baca7c7408002e37bccb2de97fc5b981677cd190
pooran-meena <153830513+pooran-meena@users.noreply.github.com>
2024-08-27 11:29:43 +0530

    Update {{name.snakeCase()}}_repo.dart
    New update 27.8.24

--- bloc_arc/__brick__/{{name.snakeCase()}}/repo/{{name.snakeCase()}}_repo.dart
+++ bloc_arc/__brick__/{{name.snakeCase()}}/repo/{{name.snakeCase()}}_repo.dart
@@ -1,3 +1,23 @@
 class {{name.pascalCase()}}Repo {
+final DioUtil _dioUtil = DioUtil();
+
+Future<{{name.pascalCase()}}Model?> getDataApi({
+  required int id,
+  }) async {
+  {{name.pascalCase()}}Model? result;
+    try {
+      final response =
+      await _dioUtil.getApi(path: ApiConfig.getData, queryParameters: {
+      'id': id,
+      });
+      if(response != null) {
+      result = {{name.pascalCase()}}Model.fromJson(response.data);
+      }
+      return result;
+    } catch (e) {
+    DebugUtils.showPrint('Exception getDataApi : $e');
+    }
+  return result;
+}
 
 }
