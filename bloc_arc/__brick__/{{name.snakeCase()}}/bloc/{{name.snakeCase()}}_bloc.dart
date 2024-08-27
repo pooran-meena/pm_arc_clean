c0fe3136927737e4b08908eac585dbe45c76de70
pooran-meena <153830513+pooran-meena@users.noreply.github.com>
2024-08-27 11:28:30 +0530

    Update {{name.snakeCase()}}_bloc.dart
    I. New update 27.8.24

--- bloc_arc/__brick__/{{name.snakeCase()}}/bloc/{{name.snakeCase()}}_bloc.dart
+++ bloc_arc/__brick__/{{name.snakeCase()}}/bloc/{{name.snakeCase()}}_bloc.dart
@@ -4,10 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class {{name.pascalCase()}}Bloc extends Bloc<{{name.pascalCase()}}Event, {{name.pascalCase()}}State> {
 {{name.pascalCase()}}Bloc() : super(const {{name.pascalCase()}}State()) {
-    on<{{name.pascalCase()}}Event>(_onGetData);
+    on<Update{{name.pascalCase()}}Event>(_onUpdate{{name.pascalCase()}});
   }
 
-  FutureOr<void> _onGetData({{name.pascalCase()}}Event event, Emitter<{{name.pascalCase()}}State> emit) async {
+  FutureOr<void> _onUpdate{{name.pascalCase()}}(Update{{name.pascalCase()}}Event event, Emitter<{{name.pascalCase()}}State> emit) async {
     emit(state.copyWith(status: {{name.pascalCase()}}StateStatus.loading));
   }
 }
