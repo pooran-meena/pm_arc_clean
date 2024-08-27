7a7653e7b177b01ab4569907438a2b4bf494ef55
pooran-meena <153830513+pooran-meena@users.noreply.github.com>
2024-08-27 11:30:15 +0530

    Update {{name.snakeCase()}}_screen.dart
    New update 27.8.24

--- bloc_arc/__brick__/{{name.snakeCase()}}/view/{{name.snakeCase()}}_screen.dart
+++ bloc_arc/__brick__/{{name.snakeCase()}}/view/{{name.snakeCase()}}_screen.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 
-class {{name.pascalCase()}}Screen extends StatefulWidget {
-  static const String id = '{{name.pascalCase()}}Screen';
-  const {{name.pascalCase()}}Screen({super.key});
+class {{name.pascalCase()}}Screen extends StatelessWidget {
+  static const id = '/{{name.pascalCase()}}Screen';
+  const {{name.pascalCase()}}Screen({super.key,});
 
   @override
-  State<{{name.pascalCase()}}Screen> createState() => _{{name.pascalCase()}}ScreenState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create:(context) => {{name.pascalCase()}}Bloc(),
+      child: _{{name.pascalCase()}}Screen(),
+    );
+  }
 }
 
-class _{{name.pascalCase()}}ScreenState extends State<{{name.pascalCase()}}Screen> {
+class _{{name.pascalCase()}}Screen extends StatefulWidget {
+  const _{{name.pascalCase()}}Screen({super.key});
+
+  @override
+  State<_{{name.pascalCase()}}Screen> createState() => _{{name.pascalCase()}}ScreenState();
+}
+
+class _{{name.pascalCase()}}ScreenState extends State<_{{name.pascalCase()}}Screen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     WidgetsBinding.instance.addPostFrameCallback((_) {
       initialCall();
     });
@@ -20,7 +31,6 @@ class _{{name.pascalCase()}}ScreenState extends State<{{name.pascalCase()}}Scree
   }
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
   }
 
@@ -30,6 +40,25 @@ class _{{name.pascalCase()}}ScreenState extends State<{{name.pascalCase()}}Scree
 
   @override
   Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: const CustomAppBar(title: AppStrings.{{name.pascalCase()}}),
+    backgroundColor: AppColors.themeColor,
+    body: BlocConsumer<{{name.pascalCase()}}Bloc, {{name.pascalCase()}}Bloc>(
+      listener: (context, state) {
+        if (state.status == {{name.pascalCase()}}StateStatus.success) {
+          // ToastCustom.showToast(msg: state.responseMessage ?? '');
+        }
+      },
+      builder: (context, state) {
+        return LoadScreen(
+          isLoading: state.status == {{name.pascalCase()}}StateStatus.loading,
+          widget: Center(
+            child: Text('{{name.pascalCase()}} Page'),
+          )
+        );
+      }
+    )
+  );
     return const Scaffold(
       body: Center(
         child: Text('{{name.pascalCase()}} Page'),
