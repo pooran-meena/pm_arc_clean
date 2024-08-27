91a7523c2c722eeb778f4d030889fa697d933be4
pooran-meena <153830513+pooran-meena@users.noreply.github.com>
2024-08-27 11:29:05 +0530

    Update {{name.snakeCase()}}_event.dart
    New update 27.8.24

--- bloc_arc/__brick__/{{name.snakeCase()}}/bloc/{{name.snakeCase()}}_event.dart
+++ bloc_arc/__brick__/{{name.snakeCase()}}/bloc/{{name.snakeCase()}}_event.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 @immutable
 abstract class {{name.pascalCase()}}Event {}
+
+class Update{{name.pascalCase()}}Event extends {{name.pascalCase()}}Event{
+  const Update{{name.pascalCase()}}Event({this.request});
+  final int? ̉value;
+}
